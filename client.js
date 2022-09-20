02fbfc6dd2b24dc01044270504ceb76113f47a0f
JPC8 <joshua.cyril88@gmail.com>
2022-09-20 12:39:36 +0530

    md

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('https://wv-chat.herokuapp.com/');
 const messages = document.getElementById('messages');
 const msgForm = document.getElementById('msgForm');
 
